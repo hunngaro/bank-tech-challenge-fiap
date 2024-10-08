2c1aca16b1525ef6ecbc52e5f653fb7e6bf1d06c
Rafael Miralha <rafamiralha1@gmail.com>
2024-10-08 20:54:29 -0300

    fix: lógica do HandleClose atualizada

--- src/app/components/formulario-cadastro/formulario-cadastro.tsx
+++ src/app/components/formulario-cadastro/formulario-cadastro.tsx
@@ -1,7 +1,8 @@
 'use client'
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 import imgCadastro from "@/app/assets/cadastro.svg"
+
 interface CadastroProps {
     isOpen: boolean
     onClose: VoidFunction
@@ -10,13 +11,13 @@ interface CadastroProps {
 
 const Cadastro: React.FC<CadastroProps> = ({ isOpen, onClose }) => {
     if (!isOpen) return null;
-    const [setShowModal] = useState(false);
-    const handleClose = (e: any) => {
-        if (e.target.id === 'wrapper') onClose();
+    const handleClose = () => {
+        onClose();
     }
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center " id="wrapper" onClick={(handleClose)}>
-            <div className="xl:w-6/12 w-5/6 h-screen flex flex-col bg-my-light-gray">
+        <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
+            <div className="fixed  inset-0 flex justify-center items-center  " id="wrapper" onClick={handleClose}></div>
+            <div className=" z-10 xl:w-5/12 w-5/6 h-screen flex flex-col bg-my-light-gray max-sm:overflow-y-auto">
                 <button className="text-black text-xl place-self-end p-4" onClick={() => onClose()}>X
                 </button>
                 <div className="flex flex-col text-black p-2 rounded justify-center items-center ">
@@ -28,28 +29,28 @@ const Cadastro: React.FC<CadastroProps> = ({ isOpen, onClose }) => {
                     <p className="py-8 text-xl font-bold">Preencha os campos abaixo para criar sua conta corrente!</p>
 
                 </div>
-                <div className="flex justify-start xl:ml-32 md:ml-20 w-3/4  p-8 ">
+                <div className="flex justify-start xl:ml-20 md:ml-20 w-3/4  p-8 ">
                     <form action="#">
                         <div>
                             <label htmlFor="name" className="block mb-2"
                             >
                                 <div className="text-black font-bold"  >Nome</div>
                             </label>
-                            <input type="name" name="name" id="name" className="bg-white border-gray-300 mb-10 p-2 rounded-lg md:w-80" placeholder="Digite seu nome Completo" />
+                            <input type="name" name="name" id="name" className="bg-white  border-gray-200 border-2 mb-10 p-2 rounded-lg md:w-80" placeholder="Digite seu nome Completo" />
                         </div>
                         <div>
                             <label htmlFor="email" className="block mb-2"
                             >
                                 <div className="text-black font-bold"  >Email</div>
                             </label>
-                            <input type="email" name="email" id="email" className="bg-white border-gray-300 mb-10 p-2 rounded-lg md:w-80" placeholder="Digite seu E-mail" />
+                            <input type="email" name="email" id="email" className="bg-white  border-gray-200 border-2 mb-10 p-2 rounded-lg md:w-80" placeholder="Digite seu E-mail" />
                         </div>
                         <div>
                             <label htmlFor="password" className="block mb-2"
                             >
                                 <div className="text-black font-bold"  >Senha</div>
                             </label>
-                            <input type="password" name="password" id="password" className="bg-white border-gray-300 p-2 rounded-lg" placeholder="Digite sua senha" />
+                            <input type="password" name="password" id="password" className="bg-white border-gray-200 border-2 p-2 rounded-lg" placeholder="Digite sua senha" />
                         </div>
                     </form>
                 </div>
--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -7,8 +7,8 @@ export default function Header() {
     const [showModal, setShowModal] = useState(false);
     return (
         <header className="flex justify-center p-4">
-            <Image src={Icon} alt="logo do Banco" className="" />
-            <h1 className="text-my-green text-2xl flex mr-9 gap-2  "> Bytebank</h1>
+            <Image src={Icon} alt="logo do Banco" className=" md: mr-4 " />
+            <h1 className="text-my-green text-2xl flex mr-9 gap-2  md:hidden lg:block"> Bytebank</h1>
             <button className="bg-my-green rounded-md p-1 mr-5 hover:opacity-70 hover:transition-opacity max-sm:hidden" onClick={() =>
                 setShowModal(true)}>Abrir minha conta
 
