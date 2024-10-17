db8abed0b9b528effce563ab74d3c9e8429c38ff
Guilherme Farias <guifarias@Guilhermes-MacBook-Pro.local>
2024-10-17 12:38:48 -0300

    feat: create dashboard page, nova tranascao component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,11 @@
+import NovaTransacao from "../components/nova-transacao/nova-tranasacao";
+import Saldo from "../components/saldo/saldo";
+
 export default function Dashboard() {
-  return <h1>Dashboard!</h1>;
+  return (
+  <div className="flex flex-col gap-6">
+    <Saldo />
+    <NovaTransacao/>
+  </div>
+  );
 }
