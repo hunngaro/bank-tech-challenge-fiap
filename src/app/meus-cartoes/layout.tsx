5a217089b3a80e60994243b7d0fa695b07969a26
Lucas Vieira <lucas_99silva@hotmail.com>
2024-10-19 23:45:08 -0300

    style: adjust sidemenu layout

--- src/app/meus-cartoes/layout.tsx
+++ src/app/meus-cartoes/layout.tsx
@@ -8,8 +8,8 @@ interface Props {
 export default function DashboardLayout({ children }: Props) {
   return (
     <div className="flex flex-col flex-1 bg-my-light-green">
-      <div className="container mx-auto flex md:flex-col lg:flex-row flex-grow gap-6 pt-6">
-        <aside className="lg:w-[180px] lg:bg-[#F5F5F5] text-black rounded-lg lg:p-8 md:py-2 lg:block md:block sm:hidden hidden">
+      <div className="container mx-auto lg:grid md:grid-cols-[180px_minmax(900px,_1fr)] lg:flex-row flex-grow gap-6 pt-6">
+        <aside className="lg:w-[180px] lg:bg-[#F5F5F5] text-black rounded-lg lg:px-6 lg:py-8 md:py-2 lg:block md:block sm:hidden hidden">
           <NavLinks />
         </aside>
 
