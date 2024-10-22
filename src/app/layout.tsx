55ac093acc5f21ec21cca6b08f35f333f1026b34
Lucas Vieira <lucas_99silva@hotmail.com>
2024-10-22 12:04:22 -0300

    style: adjust boas-vindas component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import "./globals.css";
 import Header from "./components/header/header";
 import Footer from "./components/Footer/footer";
 
-
-
 export const metadata: Metadata = {
   title: "ByteBank",
 };
