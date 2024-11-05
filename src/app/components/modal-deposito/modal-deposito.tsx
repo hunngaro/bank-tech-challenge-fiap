28581ee755b582879df3e2afbde7e3b894d6fd7d
Lucas Vieira <lucas_99silva@hotmail.com>
2024-11-05 09:36:39 -0300

    fix: adjust ModalDeposito component and menu

--- src/app/components/modal-deposito/modal-deposito.tsx
+++ src/app/components/modal-deposito/modal-deposito.tsx
@@ -1,52 +1,61 @@
 import Image from "next/image";
-import close from '@/app/assets/close-black.svg'
-import { DepositoContext, depositos, TransactionData } from "@/app/contexts/deposito-context";
+import close from "@/app/assets/close-black.svg";
+import {
+  DepositoContext,
+  depositos,
+  TransactionData,
+} from "@/app/contexts/deposito-context";
 import { FormEvent, useContext, useState } from "react";
 import { formatToReais } from "@/app/utils/format";
 
 interface Props {
-  isOpen: boolean
-  onClose: () => void
-  deposito: depositos
+  isOpen: boolean;
+  onClose: () => void;
+  deposito: depositos;
 }
 
 export function ModalDeposito({ isOpen, onClose, deposito }: Props) {
-  const { updateTransaction } = useContext(DepositoContext)
-  const [typeTransaction, setTypeTransaction] = useState(deposito.label)
-  const [date, setDate] = useState(deposito.data)
-  const [value, setValue] = useState(deposito.valor)
+  const { updateTransaction } = useContext(DepositoContext);
+  const [typeTransaction, setTypeTransaction] = useState(deposito.label);
+  const [date, setDate] = useState(deposito.data);
+  const [value, setValue] = useState(deposito.valor);
 
   const handleUpdateTransaction = async (event: FormEvent) => {
-    event.preventDefault()
+    event.preventDefault();
     try {
       const transactionData: TransactionData = {
         typeTransaction,
         date,
-        valueTransaction: value
-      }
-      await updateTransaction(deposito.id, transactionData)
-      onClose()
+        valueTransaction: value,
+      };
+      await updateTransaction(deposito.id, transactionData);
+      onClose();
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>) => {
+  const handleChange = (
+    event: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>
+  ) => {
     let { value }: any = event.target;
     if (event.target.name === "value") {
       value = formatToReais(event.target.value);
     }
-    setValue(value)
+    setValue(value);
   };
 
-  if (!isOpen) return null
+  if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center p-4">
-      <div className="fixed h-screen w-full bg-black bg-opacity-50" onClick={onClose}></div>
+      <div
+        className="fixed h-screen w-full bg-black bg-opacity-50"
+        onClick={onClose}
+      ></div>
       <div className="relative bg-white rounded-lg shadow-lg p-6 w-full max-w-md">
-        <button 
-          type="button" 
+        <button
+          type="button"
           className="absolute top-3 right-3"
           onClick={onClose}
         >
@@ -55,7 +64,13 @@ export function ModalDeposito({ isOpen, onClose, deposito }: Props) {
         <div>
           <h2 className="text-xl font-bold">Editar transação</h2>
           <form className="grid gap-2 mt-4" onSubmit={handleUpdateTransaction}>
-            <select name="tipo" id="tipo" value={typeTransaction} onChange={(e) => setTypeTransaction(e.target.value)} className="selectNewTransaction w-full h-12 pl-4 border-[1px] focus:border-my-green border-my-blue rounded-lg text-black appearance-none outline-none">
+            <select
+              name="tipo"
+              id="tipo"
+              value={typeTransaction}
+              onChange={(e) => setTypeTransaction(e.target.value)}
+              className="selectNewTransaction w-full h-12 pl-4 border-[1px] focus:border-my-green border-my-blue rounded-lg text-black appearance-none outline-none"
+            >
               <option value="cambio">Câmbio de Moeda</option>
               <option value="doc/ted">DOC/TED</option>
               <option value="emprestimo">Empréstimo e Financiamento</option>
@@ -63,14 +78,31 @@ export function ModalDeposito({ isOpen, onClose, deposito }: Props) {
               <option value="debito">Débito</option>
               <option value="credito">Crédito</option>
             </select>
-            
+
             <label htmlFor="transaction-date">Data da transação</label>
-            <input type="date" name="transaction-date" value={date} onChange={(e) => setDate(e.target.value)} className="py-3 w-full border-[1px] border-my-blue focus:border-my-green rounded-lg text-black text-center outline-none" />
+            <input
+              type="date"
+              name="transaction-date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              className="py-3 w-full border-[1px] border-my-blue focus:border-my-green rounded-lg text-black text-center outline-none"
+            />
 
             <label htmlFor="value">Valor</label>
-            <input type="text" name="value" value={formatToReais(value)} onChange={handleChange} className="py-3 w-full border-[1px] border-my-blue focus:border-my-green rounded-lg text-black text-center outline-none" />
+            <input
+              type="text"
+              name="value"
+              value={formatToReais(value)}
+              onChange={handleChange}
+              className="py-3 w-full border-[1px] border-my-blue focus:border-my-green rounded-lg text-black text-center outline-none"
+            />
 
-            <button type="submit" className="bg-my-blue hover:bg-black transition-all text-white py-3 rounded-lg mt-2">Editar transação</button>
+            <button
+              type="submit"
+              className="bg-my-blue hover:bg-black transition-all text-white py-3 rounded-lg mt-2"
+            >
+              Editar transação
+            </button>
           </form>
         </div>
       </div>
