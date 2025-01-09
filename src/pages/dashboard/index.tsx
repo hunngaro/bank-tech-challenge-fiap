4f8816966371ae27f43c0d5e6835384d8e686b82
Gui Farias <gui.farias@outlook.com.br>
2025-01-09 16:36:48 -0300

    feat: grafico ultimas transacoes

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,12 +2,14 @@ import DefaultLayout from "@/layouts/default-layout";
 import NovaTransacao from "@/components/nova-transacao/nova-transacao";
 import Saldo from "@/components/saldo/saldo";
 import { ReactElement } from "react";
+import GraficoDash from "@/components/grafico-dash/grafico-dash";
 
 export default function Dashboard() {
   return (
     <div className="flex flex-col gap-8">
       <Saldo />
       <NovaTransacao />
+      <GraficoDash/>
     </div>
   );
 }
